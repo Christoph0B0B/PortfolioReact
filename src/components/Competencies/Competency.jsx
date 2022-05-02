b5a4171543849d48621b7e0a7cc720852ceae700
Bye, Christoph-Bernhard <Christoph-Bernhard.Bye@adesso.de>
2022-05-02 17:03:21 +0200

    competencyList load data from file, competency display mixed data load from file

--- src/components/Competencies/Competency.jsx
+++ src/components/Competencies/Competency.jsx
@@ -1,31 +1,30 @@
 import PropTypes from "prop-types";
 
-export default function Competency(prop) {
+export default function Competency(prop ) {
 
- console.log("competency prop title:"+ prop.competencyData.title);
-    console.log("competency prop title:"+ prop.competencyData.title);
+    console.log("competency prop title:" + prop.name);
+    console.log("competency prop title:" + prop.competencyData.description);
 
-    return(
+    return (
 
-    <div className="col-2">
-        {/*<ul className="list-group">*/}
-        {/*    <li className="list-group-item">hobbies</li>*/}
-        {/*    <li className="list-group-item">Dapibus ac facilisis in</li>*/}
-        {/*    <li className="list-group-item">Morbi leo risus</li>*/}
-        {/*    <li className="list-group-item">Porta ac consectetur ac</li>*/}
-        {/*    <li className="list-group-item">Vestibulum at eros</li>*/}
-        {/*</ul>*/}
-
-        <dl className="row">
-            <dt className="col-sm-3">{prop.competencyData.title}</dt>
-            <dd className="col-sm-9">{prop.competencyData.content}</dd>
-            {/*<dt className="col-sm-3">prop.competencyData.title</dt>*/}
-            {/*<dd className="col-sm-9">prop.competencyData.content</dd>*/}
-        </dl>
-    </div>
+        <div className="col-2">
+            {/*<ul className="list-group">*/}
+            {/*    <li className="list-group-item">hobbies</li>*/}
+            {/*    <li className="list-group-item">Dapibus ac facilisis in</li>*/}
+            {/*    <li className="list-group-item">Morbi leo risus</li>*/}
+            {/*    <li className="list-group-item">Porta ac consectetur ac</li>*/}
+            {/*    <li className="list-group-item">Vestibulum at eros</li>*/}
+            {/*</ul>*/}
+            <h1>Competency start</h1>
+            <dl className="row">
+                <dt className="col-sm-3">{prop.competencyData.name}</dt>
+                <dd className="col-sm-9">{prop.competencyData.description}</dd>
+            </dl>
+            <h1>Competency ende</h1>
+        </div>
     );
     competencies: PropTypes.shape({
-        title: PropTypes.string,
-        content: PropTypes.array
+        name: PropTypes.string,
+        description: PropTypes.array
     })
 }
--- src/components/Competencies/CompetencyList.jsx
+++ src/components/Competencies/CompetencyList.jsx
@@ -1,48 +1,51 @@
 import {useState, useEffect} from 'react';
 import Competency from './Competency';
 import ApiService from "../../Api/ApiService";
+import data from '../../assets/simple.json';
 
 export default function CompetencyList() {
 
-    const [competencies, setCompetencies] = useState();
+    const [competencies, setCompetencies] = useState(data);
 
-    // async function fetchOnRender() {
-    //     const data = await ApiService.fetchDataFromApi('http://localhost:4000/competencies');
-    //     setCompetencies(data);
-    //     console.log("data:" + data);
+
     //
-    // }
+    // var myArray = competencies.map(function(town){
+    //     return town[2];
+    // });
 
-    useEffect(() => {
-        // fetchOnRender().then((data)=>(setCompetencies(data.json())));
-        const fetchData = async () => {
-            const response = await ApiService.fetchDataFromApi('http://localhost:4000/competencies');
-            setCompetencies(response);
-        }
-        fetchData();
-        // fetchOnRender().then((data)=>(setCompetencies(data.json())));
-        // const fetchData =  () => {
-        //     const response =  ApiService.fetchDataFromApi('http://localhost:4000/competencies');
-        //     setCompetencies(response);
-        // }
-        // fetchData();
-    }, []);
+    // useEffect(() => {
+    //     // fetchOnRender().then((data)=>(setCompetencies(data.json())));
+    //     const fetchData = async () => {
+    //         const response = await ApiService.fetchDataFromApi('http://localhost:4000/competencies');
+    //         setCompetencies(response);
+    //     }
+    //     fetchData();
+    //     // fetchOnRender().then((data)=>(setCompetencies(data.json())));
+    //     // const fetchData =  () => {
+    //     //     const response =  ApiService.fetchDataFromApi('http://localhost:4000/competencies');
+    //     //     setCompetencies(response);
+    //     // }
+    //     // fetchData();
+    // }, []);
 
 
     console.log("competencies:" + competencies);
 
+    let competencies2 = [{"name": "languages", "description": ["englisch"]},
+        {"name": "programmingLanguages", "description": ["c", "c++" , "java", "php"]},
+        {"name": "certifications", "description": ["ISTQB Foundation Level", "Prince2 Foundation Level" ]}]
 
-    // console.log("competencies.languages.title: " + competencies.languages.title);
     return (
         <>
             {
                 competencies.map(
-                    (skill, index) => {
-                    console.log("skill:"+ skill)
-                      return  <Competency key={index} competencyData={skill[index]}/>;
-                }
-
-            )}
+                    (order, index) => {
+                        console.log("order.name;:" + order.name);
+                        console.log("order.description:" + order.description);
+                        return <Competency key={index} competencyData={order}/>;
+                    }
+                )}
+            }
         </>
     );
 }
